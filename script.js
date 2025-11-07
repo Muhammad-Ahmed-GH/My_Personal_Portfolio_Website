61f9e541fff1e4efd28bd6c32e48d32a11dea72d
Muhammad-Ahmed-GH <Muhammad.Ahmed.Connect@gmail.com>
2025-11-07 22:07:26 +0200

    feat(header): add click event to toggle menu

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-// function switchPageTheme() {
-//   if (darkButton.classList.contains('dark')) {
-//     darkButton.title = "switch to Dark Mode";
-//     darkButton.classList.toggle('dark', false);
-//     root.classList.toggle('dark', false);
-//   }
-//   else {
-//     darkButton.title = "switch to Light Mode";
-//     darkButton.classList.add('dark');
-//     root.classList.add('dark');
-//   }
-// }
-
-function initPageTheme() {
-  if (prefersDark.matches) {
-    // darkButton.classList.add('dark');
-    // darkButton.title = 'switch to Dark Mode';
-    root.classList.add('dark');
-  }
-  else {
-    // darkButton.classList.toggle('dark', false);
-    // darkButton.title = 'switch to Light Mode';
-    root.classList.toggle('dark', false);
-  }
-}
-
-// let darkButton = document.querySelector('.dark-btn');
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
 let root = document.documentElement;
 
-initPageTheme();
-// darkButton.addEventListener('click', switchPageTheme);
-// prefersDark.addEventListener('change', initPageTheme);
+// setting page theme color
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+if (prefersDark.matches) {
+  root.classList.add('dark');
+} else {
+  root.classList.toggle('dark', false);
+}
+
+// menu toggle button
+let menuBtn = document.querySelector('nav .bars');
+menuBtn.onclick = function () {
+  if(this.parentElement.classList.contains('open')) {
+    this.parentElement.classList.remove('open');
+  } else {
+    this.parentElement.classList.add('open');
+  }
+}
