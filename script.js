7b8885039cb5f396600110181ab6ef443f1bd7d2
Muhammad-Ahmed-GH <Muhammad.Ahmed.Connect@gmail.com>
2025-11-08 21:58:34 +0200

    trivial: formatting files

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let root = document.documentElement;
 
 // control color theme
-let media = window.matchMedia('(prefers-color-scheme: dark)');
-if (media.matches)
-  root.classList.add('dark');
-media.onchange = () => root.classList.toggle('dark');
+let media = window.matchMedia("(prefers-color-scheme: dark)");
+if (media.matches) root.classList.add("dark");
+media.onchange = () => root.classList.toggle("dark");
 
 // menu toggle button
-let menuBtn = document.querySelector('nav .bars');
-menuBtn.onclick = () => menuBtn.closest('nav').classList.toggle('open');
+let menuBtn = document.querySelector("nav .bars");
+menuBtn.onclick = () => menuBtn.closest("nav").classList.toggle("open");
