7c0f155113d4f6662898eb9442f24c2beac34566
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-16 13:12:13 +0200

    refactor: add scroll button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Content from "./components/Content";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import ScrollButton from "./components/UI/ScrollButton";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Content />
       <Footer />
+      <ScrollButton />
     </>
   );
 }
