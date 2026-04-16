ba00e874865b7a785bc3dd2c4a49b867de4099cf
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-04-16 08:10:13 +0200

    style: make fonts responsive in sections

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -7,25 +7,27 @@ export default function Landing() {
   return (
     <section
       id="main"
-      className="p-0 min-h-auto text-center text-[14px] md:text-[1rem] md:text-start"
+      className="p-0 min-h-auto text-center text-[1em] md:text-start"
     >
       <Container className="container px-4 mx-auto 2xl:max-w-[73rem] pt-header-h min-h-screen flex flex-col justify-center">
-        <p className="text-primary text-[1.1em] font-light uppercase sm:text-[1.2em]">
+        <p className="text-primary text-[1em] font-light uppercase sm:text-[1.2em]">
           Hey there, thanks for passing by
-          <span className="block m-[20px_0_30px] text-heading [text-transform:none] font-rufina font-bold text-[2.3em] mt-[15px] leading-[1.1] md:mt-[20px] md:leading-[inherit] sm:text-[3em]">
+          <span className="block m-[20px_0_30px] text-heading [text-transform:none] font-rufina font-bold text-[3em] mt-[15px] leading-[1.1] md:mt-[20px] md:leading-[inherit]">
             I'm Muhammad
             <br />
             Frontend Developer
           </span>
         </p>
 
-        <p className="text-[1em] mb-[30px] max-w-[650px] text-text-light leading-[1.5] sm:text-[1.1em]">
+        <p className="text-[1.1em] mb-[30px] max-w-[650px] text-text-light leading-[1.5] ">
           I'm an aspiring web developer, and a computer science student
         </p>
 
         <div className="flex flex-col items-center gap-[20px] mb-[20px] md:flex-row">
           <MainButton href="#projects">View my work</MainButton>
-          <AltButton href={resumeLink} target="_blank" rel="noopener">My resume</AltButton>
+          <AltButton href={resumeLink} target="_blank" rel="noopener">
+            My resume
+          </AltButton>
         </div>
 
         <div className="flex justify-center md:justify-start">
